1880aed3184458f515cf0b7edfe8ad087b8ae2e4
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2019-05-11 21:36:11 +1000

    Тест вывода целых чисел

---
+++ test/model3.ml
@@ -0,0 +1,14 @@
+for i = -1000 to 1000 do
+    print_int (i);
+    print_string " ";
+done;;
+
+print_newline ();
+print_newline ();
+
+for i = -1000 to 1000 do
+    print_int (i * 1234567890123);
+    print_string " ";
+done;;
+
+print_newline ();
