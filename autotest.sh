830733cfaaac7d8a199556152b847e641b240059
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2019-06-09 00:18:40 +1000

    autotest.sh - скрипт для запуска тестов

---
+++ autotest.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+testsuite="../ocaml/testsuite/tests"
+tests="basic"
+
+tmpdir=`mktemp -d --tmpdir microcamltest.XXX`
+
+for testdir in ${tests}
+do
+    echo -e "\033[1mКаталог ${testdir}:\033[22m"
+    sources=`ls -1 ${testsuite}/${testdir}/*ml`
+    for filepath in ${sources}
+    do
+        filename=${filepath##*/}
+        echo -ne "${filename} \t"
+        cp ${filepath} ${tmpdir}
+        cp ${filepath%.ml}.reference ${tmpdir}
+        bytecode=${tmpdir}/${filename%.ml}
+        ocamlc ${tmpdir}/${filename} -o ${bytecode}
+        rm ${tmpdir}/*.cm*
+        ./microcaml ${bytecode} > ${bytecode}.output
+        diff -u ${bytecode}.output ${bytecode}.reference && echo '+' || exit
+    done
+done
+
+rm --recursive --force $tmpdir
