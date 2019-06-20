35c0ba71a84c9b9f21d6c4bff3e4d6bd77fdc787
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2019-06-20 18:34:11 +1000

    Удалены генераторы gen_instruct.sh gen_primitives.sh и primitives
    Использовались на начальной стадии для генерации заготовок
    instruct.asm primitives.asm primitives.inc. Давно без надобности.

--- gen_primitives.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-echo 'C_primitives equ \' > primitives.inc.orig
-sed -e 's/.*/\t&,\\/' primitives >> primitives.inc.orig
-echo '' >> primitives.inc.orig
-
-sed -e 's/.*/C_primitive &\n\nend C_primitive\n\n\n/' primitives > primitives.fasm.orig
-
-
