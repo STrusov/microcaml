a64a01387713a71d2ebf6a5ebb1cf399a1ca841e
Sergei A. Trusov <sergei.a.trusov@ya.ru>
2019-06-21 00:35:59 +1000

    gc_ctrl.asm: процедуры управления сборщиком мусора выделены из primitives.asm

---
+++ gc_ctrl.asm
@@ -0,0 +1,83 @@
+; Процедуры (C примитивы) управления сборщиком мусора.
+
+
+C_primitive caml_gc_stat
+end C_primitive
+
+
+C_primitive caml_gc_quick_stat
+end C_primitive
+
+
+C_primitive caml_gc_minor_words
+end C_primitive
+
+
+C_primitive caml_gc_counters
+end C_primitive
+
+
+C_primitive caml_gc_huge_fallback_count
+end C_primitive
+
+
+C_primitive caml_gc_get
+end C_primitive
+
+
+C_primitive caml_gc_set
+end C_primitive
+
+
+caml_gc_minor := caml_gc_full_major
+
+
+C_primitive caml_gc_major
+end C_primitive
+
+
+; EDI - не используется.
+C_primitive caml_gc_full_major
+if HEAP_GC
+	mov	alloc_small_ptr, alloc_small_ptr_backup
+	call	heap_mark_compact_gc
+	mov	alloc_small_ptr_backup, alloc_small_ptr
+end if
+	ret
+end C_primitive
+
+
+C_primitive caml_gc_major_slice
+end C_primitive
+
+
+C_primitive caml_gc_compaction
+end C_primitive
+
+
+C_primitive caml_get_minor_free
+end C_primitive
+
+
+C_primitive caml_get_major_bucket
+end C_primitive
+
+
+C_primitive caml_get_major_credit
+end C_primitive
+
+
+C_primitive caml_runtime_variant
+end C_primitive
+
+
+C_primitive caml_runtime_parameters
+end C_primitive
+
+
+C_primitive caml_ml_enable_runtime_warnings
+end C_primitive
+
+
+C_primitive caml_ml_runtime_warnings_enabled
+end C_primitive
--- primitives.asm
+++ primitives.asm
@@ -44,6 +44,7 @@ include 'ints.asm'
 include 'floats.asm'
 include 'obj.asm'
 include 'sys.asm'
+include 'gc_ctrl.asm'
 
 
 C_primitive caml_add_debug_info
@@ -215,79 +216,6 @@ end C_primitive
 
 
 
-C_primitive caml_gc_compaction
-
-end C_primitive
-
-
-
-C_primitive caml_gc_counters
-
-end C_primitive
-
-
-; EDI - не используется.
-C_primitive caml_gc_full_major
-if HEAP_GC
-	mov	alloc_small_ptr, alloc_small_ptr_backup
-	call	heap_mark_compact_gc
-	mov	alloc_small_ptr_backup, alloc_small_ptr
-end if
-	ret
-end C_primitive
-
-
-
-C_primitive caml_gc_get
-
-end C_primitive
-
-
-
-C_primitive caml_gc_huge_fallback_count
-
-end C_primitive
-
-
-
-C_primitive caml_gc_major
-
-end C_primitive
-
-
-
-C_primitive caml_gc_major_slice
-
-end C_primitive
-
-
-caml_gc_minor := caml_gc_full_major
-
-
-C_primitive caml_gc_minor_words
-
-end C_primitive
-
-
-
-C_primitive caml_gc_quick_stat
-
-end C_primitive
-
-
-
-C_primitive caml_gc_set
-
-end C_primitive
-
-
-
-C_primitive caml_gc_stat
-
-end C_primitive
-
-
-
 C_primitive caml_get_current_callstack
 
 end C_primitive
@@ -313,24 +241,6 @@ end C_primitive
 
 
 C_primitive caml_get_global_data
-
-end C_primitive
-
-
-
-C_primitive caml_get_major_bucket
-
-end C_primitive
-
-
-
-C_primitive caml_get_major_credit
-
-end C_primitive
-
-
-
-C_primitive caml_get_minor_free
 
 end C_primitive
 
@@ -424,11 +334,6 @@ C_primitive caml_ml_close_channel
 
 end C_primitive
 
-
-
-C_primitive caml_ml_enable_runtime_warnings
-
-end C_primitive
 
 
 ; RDI - виртуальный канал
@@ -824,12 +729,6 @@ end C_primitive
 
 
 
-C_primitive caml_ml_runtime_warnings_enabled
-
-end C_primitive
-
-
-
 C_primitive caml_ml_seek_in
 
 end C_primitive
@@ -953,18 +852,6 @@ end C_primitive
 
 
 C_primitive caml_remove_debug_info
-
-end C_primitive
-
-
-
-C_primitive caml_runtime_parameters
-
-end C_primitive
-
-
-
-C_primitive caml_runtime_variant
 
 end C_primitive
 
